9f88df31dfaf304c545931f82e625a27b74e9ad1
Julien kellerhals <julien@kellerhals.xyz>
2024-10-06 18:58:50 +0000

    Adds lambda cdk.

--- lib/auto-ae-scripts-stack.ts
+++ lib/auto-ae-scripts-stack.ts
@@ -6,12 +6,14 @@ export class AutoAeScriptsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Update world
+
     const updateWorld = new lambda.DockerImageFunction(this, "updateWorld", {
       code: lambda.DockerImageCode.fromImageAsset("./image", {
         file: "Dockerfile-update-world",
       }),
-      memorySize: 1024,
-      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(5),
       architecture: lambda.Architecture.ARM_64,
     });
 
@@ -24,8 +26,92 @@ export class AutoAeScriptsStack extends cdk.Stack {
       },
     });
 
-    new cdk.CfnOutput(this, "FunctionUrlValue", {
+    new cdk.CfnOutput(this, "updateWorldUrl", {
       value: updateWorldUrl.url,
+    });
+
+    // Update session token
+
+    const updateSessionToken = new lambda.DockerImageFunction(
+      this,
+      "updateSessionToken",
+      {
+        code: lambda.DockerImageCode.fromImageAsset("./image", {
+          file: "Dockerfile-update-session-token",
+        }),
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(5),
+        architecture: lambda.Architecture.ARM_64,
+      }
+    );
+
+    const updateSessionTokenUrl = updateSessionToken.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+      cors: {
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "updateSessionTokenUrl", {
+      value: updateSessionTokenUrl.url,
+    });
+
+    // Update aircraft
+
+    const updateAircraft = new lambda.DockerImageFunction(
+      this,
+      "updateAircraft",
+      {
+        code: lambda.DockerImageCode.fromImageAsset("./image", {
+          file: "Dockerfile-update-aircraft",
+        }),
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(5),
+        architecture: lambda.Architecture.ARM_64,
+      }
+    );
+
+    const updateAircraftUrl = updateAircraft.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+      cors: {
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "updateAircraftUrl", {
+      value: updateAircraftUrl.url,
+    });
+
+    // Run config
+
+    const runConfiguration = new lambda.DockerImageFunction(
+      this,
+      "runConfiguration",
+      {
+        code: lambda.DockerImageCode.fromImageAsset("./image", {
+          file: "Dockerfile-run-configuration",
+        }),
+        memorySize: 256,
+        timeout: cdk.Duration.seconds(20),
+        architecture: lambda.Architecture.ARM_64,
+      }
+    );
+
+    const runConfigurationUrl = runConfiguration.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+      cors: {
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "runConfigurationUrl", {
+      value: runConfigurationUrl.url,
     });
   }
 }
