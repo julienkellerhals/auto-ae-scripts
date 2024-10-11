5e95684aa6028896917bbed9a25f117fc07cb42d
Julien kellerhals <julien@kellerhals.xyz>
2024-10-11 20:03:36 +0000

    Adds script run.

--- image/update_aircraft.py
+++ image/update_aircraft.py
@@ -1,13 +1,13 @@
 from src.api import get_aircraft_stats
 from models.accounts import get_account_by_id
-from models.aircraft import add_aircraft
+from models.aircraft import update_aircraft
 
 
 def main(account_id: int, user_id: int) -> None:
     account = get_account_by_id(account_id)
 
     aircraft_stats_df = get_aircraft_stats(account.session_id)
-    add_aircraft(account_id, aircraft_stats_df, user_id)
+    update_aircraft(account_id, aircraft_stats_df, user_id)
 
 
 def handler(event, context):
@@ -16,3 +16,9 @@ def handler(event, context):
         int(event["user_id"]),
     )
     return {"statusCode": 200, "body": event}
+
+
+if __name__ == "__main__":
+    ACCOUNT_ID = 0
+    USER_ID = 0
+    main(ACCOUNT_ID, USER_ID)
